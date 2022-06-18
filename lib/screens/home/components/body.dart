787ac92b2030dbdc2c91a509d9f43a6ff6c06a46
999 <hyeon@imweb.me>
2022-06-19 00:04:58 +0900

    feat: section title header

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -2,100 +2,90 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/constans.dart';
 import 'package:flutter_svg/svg.dart';
 
+import 'header_with_searchbox.dart';
+
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // It will previe use total height and width of our screen
     Size size = MediaQuery.of(context).size;
+    // it enable scrolling on small device
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: Row(
+              children: [
+                TitleWitchCustomUnderline(text: "Recomended"),
+                Spacer(),
+                TextButton(
+                  onPressed: () {},
+                  child: Text("More"),
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(kPrimaryColor),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        side: BorderSide(color: kPrimaryColor),
+                      ),
+                    ),
+                  ),
+                )
+                // FlatButton(
+                //   shape: RoundedRectangleBorder(
+                //     borderRadius: BorderRadius.circular(20),
+                //   ),
+                //   color: kPrimaryColor,
+                //   onPressed: () {},
+                //   child: Text("More"),
+                // )
+              ],
+            ),
+          )
         ],
       ),
     );
   }
 }
 
-class HeaderWithSearchBox extends StatelessWidget {
-  const HeaderWithSearchBox({
+class TitleWitchCustomUnderline extends StatelessWidget {
+  const TitleWitchCustomUnderline({
     Key? key,
-    required this.size,
+    required this.text,
   }) : super(key: key);
 
-  final Size size;
+  final String text;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: size.height * 0.2,
-      child: Stack(children: <Widget>[
-        Container(
-          padding: const EdgeInsets.only(
-            left: kDefaultPadding,
-            right: kDefaultPadding,
-            bottom: 36 + kDefaultPadding,
-          ),
-          height: size.height * 0.2 - 27,
-          decoration: const BoxDecoration(
-            color: kPrimaryColor,
-            borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(36),
-                bottomRight: Radius.circular(36)),
-          ),
-          child: Row(
-            children: <Widget>[
-              Text(
-                "Hi Uishopy!",
-                style: Theme.of(context).textTheme.headline5?.copyWith(
-                    color: Colors.white, fontWeight: FontWeight.bold),
-              ),
-              const Spacer(),
-              Image.asset("assets/images/logo.png"),
-            ],
-          ),
-        ),
-        Positioned(
-          bottom: 0,
-          left: 0,
-          right: 0,
-          child: Container(
-            margin: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-            height: 54,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(
-                  offset: const Offset(0, 10),
-                  blurRadius: 50,
-                  color: kPrimaryColor.withOpacity(0.23),
-                )
-              ],
-            ),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    onChanged: (value) {},
-                    decoration: InputDecoration(
-                      hintText: "Search2",
-                      hintStyle:
-                          TextStyle(color: kPrimaryColor.withOpacity(0.5)),
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      // surffix가 SVG에서 제대로 작동하지 않습니다.
-                      // 그것이 우리가 행을 사용하는 이유입니다
-                      // suffixIcon: SvgPicture.asset("assets/icons/search.svg"),
-                    ),
-                  ),
-                ),
-                SvgPicture.asset("assets/icons/search.svg"),
-              ],
+      height: 24,
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              text,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
-        ),
-      ]),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: const EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
