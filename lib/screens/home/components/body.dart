515c7bd92057cefb2da6da96f7ff6f00fa8c5326
999 <hyeon@imweb.me>
2022-06-20 06:58:02 +0900

    feat: refactoring RecomendsPlant

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -3,6 +3,7 @@ import 'package:flutter_application_1/constans.dart';
 import 'package:flutter_svg/svg.dart';
 
 import 'header_with_searchbox.dart';
+import 'recommend_plants.dart';
 import 'title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
@@ -15,119 +16,10 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size),
-          TitleWithMoreBtn(
-            title: "Recommended",
-            press: () {},
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: <Widget>[
-                RecomendPlantCard(
-                  image: "assets/images/image_1.png",
-                  title: "Samantha",
-                  country: "Russia",
-                  price: 440,
-                  press: () {},
-                ),
-                RecomendPlantCard(
-                  image: "assets/images/image_2.png",
-                  title: "Angelica",
-                  country: "Russia",
-                  price: 440,
-                  press: () {},
-                ),
-                RecomendPlantCard(
-                  image: "assets/images/image_3.png",
-                  title: "Samantha",
-                  country: "Russia",
-                  price: 440,
-                  press: () {},
-                ),
-              ],
-            ),
-          )
+          TitleWithMoreBtn(title: "Recommended", press: () {}),
+          const RecommendsPlants(),
         ],
       ),
     );
-  }
-}
-
-class RecomendPlantCard extends StatelessWidget {
-  const RecomendPlantCard({
-    Key? key,
-    required this.image,
-    required this.title,
-    required this.country,
-    required this.price,
-    required this.press,
-  }) : super(key: key);
-
-  final String image, title, country;
-  final int price;
-  final void Function() press;
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-
-    return Container(
-        margin: EdgeInsets.only(
-          left: kDefaultPadding,
-          top: kDefaultPadding / 2,
-          bottom: kDefaultPadding * 2.5,
-        ),
-        width: size.width * 0.4,
-        child: Column(
-          children: <Widget>[
-            Image.asset(image),
-            GestureDetector(
-              onTap: press,
-              child: Container(
-                  padding: EdgeInsets.all(kDefaultPadding / 2),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10),
-                      bottomRight: Radius.circular(10),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.23),
-                      ),
-                    ],
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      RichText(
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: "$title\n".toUpperCase(),
-                              style: Theme.of(context).textTheme.button,
-                            ),
-                            TextSpan(
-                              text: "$country".toUpperCase(),
-                              style: TextStyle(
-                                color: kPrimaryColor.withOpacity(0.5),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      Spacer(),
-                      Text(
-                        '\$$price',
-                        style: Theme.of(context).textTheme.button?.copyWith(
-                              color: kPrimaryColor,
-                            ),
-                      ),
-                    ],
-                  )),
-            ),
-          ],
-        ));
   }
 }
---
+++ lib/screens/home/components/recommend_plants.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/constans.dart';
+
+class RecommendsPlants extends StatelessWidget {
+  const RecommendsPlants({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          RecommendPlantCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {},
+          ),
+          RecommendPlantCard(
+            image: "assets/images/image_2.png",
+            title: "Angelica",
+            country: "Russia",
+            price: 440,
+            press: () {},
+          ),
+          RecommendPlantCard(
+            image: "assets/images/image_3.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RecommendPlantCard extends StatelessWidget {
+  const RecommendPlantCard({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.country,
+    required this.price,
+    required this.press,
+  }) : super(key: key);
+
+  final String image, title, country;
+  final int price;
+  final void Function() press;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+        margin: const EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding * 2.5,
+        ),
+        width: size.width * 0.4,
+        child: Column(
+          children: <Widget>[
+            Image.asset(image),
+            GestureDetector(
+              onTap: press,
+              child: Container(
+                  padding: const EdgeInsets.all(kDefaultPadding / 2),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(0.23),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "$title\n".toUpperCase(),
+                              style: Theme.of(context).textTheme.button,
+                            ),
+                            TextSpan(
+                              text: "$country".toUpperCase(),
+                              style: TextStyle(
+                                color: kPrimaryColor.withOpacity(0.5),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        '\$$price',
+                        style: Theme.of(context).textTheme.button?.copyWith(
+                              color: kPrimaryColor,
+                            ),
+                      ),
+                    ],
+                  )),
+            ),
+          ],
+        ));
+  }
+}
