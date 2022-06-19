2903afa862ef059d57fb3433ecb563ce505366ef
999 <hyeon@imweb.me>
2022-06-20 07:04:09 +0900

    feat: FeaturePlant Card
    https://youtu.be/LN668OAUrK4\?t\=1045

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/constants.dart';
 
 import 'header_with_searchbox.dart';
 import 'recommend_plants.dart';
@@ -9,7 +10,7 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // It will preview use total height and width of our screen
+    // It will previe use total height and width of our screen
     Size size = MediaQuery.of(context).size;
     // it enable scrolling on small device
     return SingleChildScrollView(
@@ -18,7 +19,46 @@ class Body extends StatelessWidget {
           HeaderWithSearchBox(size: size),
           TitleWithMoreBtn(title: "Recommended", press: () {}),
           const RecommendsPlants(),
+          TitleWithMoreBtn(title: "Featured Plants", press: () {}),
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_1.png",
+            press: () {},
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class FeaturePlantCard extends StatelessWidget {
+  const FeaturePlantCard({
+    Key? key,
+    required this.image,
+    required this.press,
+  }) : super(key: key);
+
+  final String image;
+  final void Function() press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: const EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding / 2,
+        ),
+        width: size.width * 0.8,
+        height: 185,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            image: AssetImage(image),
+          ),
+        ),
       ),
     );
   }
