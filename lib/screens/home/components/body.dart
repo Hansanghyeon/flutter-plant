563a6ea69d527e01e6de6ff1bc631de3f134cbe7
999 <hyeon@imweb.me>
2022-06-20 06:40:23 +0900

    feat: RecommandCard 변수

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -20,63 +20,94 @@ class Body extends StatelessWidget {
             press: () {},
           ),
           // it will cover 40% of our total width
-          Container(
-              margin: EdgeInsets.only(
-                left: kDefaultPadding,
-                top: kDefaultPadding / 2,
-                bottom: kDefaultPadding * 2.5,
-              ),
-              width: size.width * 0.4,
-              child: Column(
-                children: <Widget>[
-                  Image.asset("assets/images/image_1.png"),
-                  Container(
-                      padding: EdgeInsets.all(kDefaultPadding / 2),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(10),
-                          bottomRight: Radius.circular(10),
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset(0, 10),
-                            blurRadius: 50,
-                            color: kPrimaryColor.withOpacity(0.23),
-                          ),
-                        ],
-                      ),
-                      child: Row(
-                        children: <Widget>[
-                          RichText(
-                            text: TextSpan(
-                              children: [
-                                TextSpan(
-                                  text: "Samantha\n".toUpperCase(),
-                                  style: Theme.of(context).textTheme.button,
-                                ),
-                                TextSpan(
-                                  text: "Russia".toUpperCase(),
-                                  style: TextStyle(
-                                    color: kPrimaryColor.withOpacity(0.5),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                          Spacer(),
-                          Text(
-                            '\$440',
-                            style: Theme.of(context).textTheme.button?.copyWith(
-                                  color: kPrimaryColor,
-                                ),
-                          ),
-                        ],
-                      )),
-                ],
-              ))
+          RecomendPlantCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {},
+          )
         ],
       ),
     );
+  }
+}
+
+class RecomendPlantCard extends StatelessWidget {
+  const RecomendPlantCard({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.country,
+    required this.price,
+    required this.press,
+  }) : super(key: key);
+
+  final String image, title, country;
+  final int price;
+  final void Function() press;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+        margin: EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding * 2.5,
+        ),
+        width: size.width * 0.4,
+        child: Column(
+          children: <Widget>[
+            Image.asset(image),
+            GestureDetector(
+              onTap: press,
+              child: Container(
+                  padding: EdgeInsets.all(kDefaultPadding / 2),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(0.23),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "$title\n".toUpperCase(),
+                              style: Theme.of(context).textTheme.button,
+                            ),
+                            TextSpan(
+                              text: "$country".toUpperCase(),
+                              style: TextStyle(
+                                color: kPrimaryColor.withOpacity(0.5),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        '\$$price',
+                        style: Theme.of(context).textTheme.button?.copyWith(
+                              color: kPrimaryColor,
+                            ),
+                      ),
+                    ],
+                  )),
+            ),
+          ],
+        ));
   }
 }
